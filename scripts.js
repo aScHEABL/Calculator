36883c77b72b79a075c40d3d17953064062929ee
aScHEABL <natothun@gmail.com>
2022-06-23 20:59:47 +0800

    feat: add arithmetic operator

--- scripts.js
+++ scripts.js
@@ -7,32 +7,59 @@ const screenDisplay_DOM = document.querySelector(`#screen-display`);
 const keys_DOM = keysArray.map((DOM_ID) => document.getElementById(`${DOM_ID}`));
 
 screenDisplay_DOM.textContent = ``;
+let operatorTriggered = false;
 
 keys_DOM.forEach((key, index) => {
     key.addEventListener(`click`, () => {
-        let seeIfKeyIsNum = (index <= 10) ? true : false;
-        if (seeIfKeyIsNum == true) screenDisplay_DOM.textContent += `${keysArray[index]}`;
+
+        let operatorIsClicked = (operator) => {
+            console.log(`Operator button is clicked!`);
+            let numberStr = screenDisplay_DOM.textContent;
+            operatorTriggered = true;
+        };        
+
+        if (operatorTriggered === true) {
+            screenDisplay_DOM.textContent = ``;
+        };
+
+        const seeIfKeyIsNum = (index <= 10) ? true : false;
+        if (seeIfKeyIsNum === true) {
+            screenDisplay_DOM.textContent += `${keysArray[index]}`;
+            operatorTriggered = false;
+        };
+        
         switch (index) {
-            case 16: // if "AC" button is pressed, clear both operator and screen display.
+            case 16: // "AC" button is pressed, clear both operator and screen display.
                 operatorDisplay_DOM.textContent = ``;
                 screenDisplay_DOM.textContent = ``;
                 break;
-            case 17: // if "C" button is pressed, backspace 1 digit from the screen display.
+            case 17: // "C" button is pressed, backspace 1 digit from the screen display.
                 screenDisplay_DOM.textContent = screenDisplay_DOM.textContent.slice(0, screenDisplay_DOM.textContent.length - 1);
                 break;
-            case 12: // if "+" button is pressed.
+            case 12: // "+" button is pressed.
                 operatorDisplay_DOM.textContent = `${keysArray[index]}`;
+                operatorIsClicked(index);
                 break;
-            case 13: // if "-" button is pressed.
+            case 13: // "-" button is pressed.
                 operatorDisplay_DOM.textContent = `${keysArray[index]}`;
+                operatorIsClicked(index);
                 break;
-            case 14: // if "*" button is pressed.
+            case 14: // "*" button is pressed.
                 operatorDisplay_DOM.textContent = `${keysArray[index]}`;
+                operatorIsClicked(index);
                 break;
-            case 15: // if "/" button is pressed.
+            case 15: // "/" button is pressed.
                 operatorDisplay_DOM.textContent = `${keysArray[index]}`;
+                operatorIsClicked(index);
                 break;
-            
+            case 11: // "=" button is pressed.
+                calculateTheResult();
+                break;
         };
     });
 });
+
+
+let calculateTheResult = () => {
+
+};
