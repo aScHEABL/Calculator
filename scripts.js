9fc5e0434ea088346f14c6efe069dbb8f05f6c44
aScHEABL <natothun@gmail.com>
2022-06-24 15:09:46 +0800

    feat: add various calculations

--- scripts.js
+++ scripts.js
@@ -7,66 +7,90 @@ const screenDisplay_DOM = document.querySelector(`#screen-display`);
 const keys_DOM = keysArray.map((DOM_ID) => document.getElementById(`${DOM_ID}`));
 
 screenDisplay_DOM.textContent = ``;
+let firstNumberString = ``;
+let secondNumberString = ``;
+let result = 0;
+let operatorIndexFromKeysArray = ``
+let operatorTriggered = false;
 
-let operatorIndex = 0;
-let firstValue = 0;
-let secondValue = 0;
-let operatorIsClicked = false;
+keys_DOM.forEach((key, index) => {
+    key.addEventListener(`click`, () => {
 
-const operatorClicked = () => {
-    console.log(`operator index is ${operatorIndex}`);
-    operatorIsClicked = true;
-};
+        let operatorIsClicked = (operatorIndex) => {
+            console.log(`Operator button is clicked!`);
+            firstNumberString = screenDisplay_DOM.textContent;
+            operatorIndexFromKeysArray = operatorIndex;
+            operatorTriggered = true;
+        };
 
-const calculateTheResult = () => {
-    
-};
+        let calculateTheResult = () => {
+            secondNumberString = screenDisplay_DOM.textContent;
+            // convert firstNumberString and secondNumberString from string to number.
+            let firstValue = +firstNumberString;
+            let secondValue = +secondNumberString;
+            // calculate the result.
+            switch (keysArray[operatorIndexFromKeysArray]) {
+                case "+" :
+                console.log(`+ calculation`);
+                result = firstValue + secondValue;
+                console.log(`result is ${result}`);
+                break;
+                case "-" :
+                console.log(`- calculation`);
+                result = firstValue - secondValue;
+                break;
+                case "*" :
+                console.log(`* calculation`);
+                result = firstValue * secondValue;
+                break;
+                case "/" :
+                console.log(`/ calculation`);
+                result = firstValue / secondValue;
+                break;
+            }
+            operatorDisplay_DOM.textContent = ``;
+            screenDisplay_DOM.textContent = `${result}`;
+            console.log(result);
+            return result;
+        };
 
-const ExecuteEventListner = () => {
-    keys_DOM.forEach((key, index) => {
-        key.addEventListener(`click`, () => {
-            const seeIfKeyIsNum = (index <= 10) ? true : false;
-            if (seeIfKeyIsNum === true) {
-                screenDisplay_DOM.textContent += `${keysArray[index]}`
-                if (operatorIsClicked === true) {
-                    operatorIsClicked = false;
-                    screenDisplay_DOM.textContent = ``;
-                };            
-            };
-            switch (index) {
-                case 16: // "AC" button is pressed, clear both operator and screen display.
-                    operatorDisplay_DOM.textContent = ``;
-                    screenDisplay_DOM.textContent = ``;
-                    break;
-                case 17: // "C" button is pressed, backspace 1 digit from the screen display.
-                    screenDisplay_DOM.textContent = screenDisplay_DOM.textContent.slice(0, screenDisplay_DOM.textContent.length - 1);
-                    break;
-                case 12: // "+" button is pressed.
-                    operatorDisplay_DOM.textContent = `${keysArray[index]}`;
-                    operatorIndex = index;
-                    operatorClicked();
-                    break;
-                case 13: // "-" button is pressed.
-                    operatorDisplay_DOM.textContent = `${keysArray[index]}`;
-                    operatorIndex = index;
-                    operatorClicked();
-                    break;
-                case 14: // "*" button is pressed.
-                    operatorDisplay_DOM.textContent = `${keysArray[index]}`;
-                    operatorIndex = index;
-                    operatorClicked();
-                    break;
-                case 15: // "/" button is pressed.
-                    operatorDisplay_DOM.textContent = `${keysArray[index]}`;
-                    operatorIndex = index;
-                    operatorClicked();
-                    break;
-                case 11: // "=" button is pressed.
-                    calculateTheResult();
-                    break;
-            };
-        });
+        if (operatorTriggered === true) {
+            screenDisplay_DOM.textContent = ``;
+        };
+
+        const seeIfKeyIsNum = (index <= 10) ? true : false;
+        if (seeIfKeyIsNum === true) {
+            screenDisplay_DOM.textContent += `${keysArray[index]}`;
+            operatorTriggered = false;
+        };
+
+        switch (index) {
+            case 16: // "AC" button is pressed, clear both operator and screen display.
+                operatorDisplay_DOM.textContent = ``;
+                screenDisplay_DOM.textContent = ``;
+                break;
+            case 17: // "C" button is pressed, backspace 1 digit from the screen display.
+                screenDisplay_DOM.textContent = screenDisplay_DOM.textContent.slice(0, screenDisplay_DOM.textContent.length - 1);
+                break;
+            case 12: // "+" button is pressed.
+                operatorDisplay_DOM.textContent = `${keysArray[index]}`;
+                operatorIsClicked(index);
+                break;
+            case 13: // "-" button is pressed.
+                operatorDisplay_DOM.textContent = `${keysArray[index]}`;
+                operatorIsClicked(index);
+                break;
+            case 14: // "*" button is pressed.
+                operatorDisplay_DOM.textContent = `${keysArray[index]}`;
+                operatorIsClicked(index);
+                break;
+            case 15: // "/" button is pressed.
+                operatorDisplay_DOM.textContent = `${keysArray[index]}`;
+                operatorIsClicked(index);
+                break;
+            case 11: // "=" button is pressed.
+                calculateTheResult();
+                break;
+        };
     });
-}
-
-ExecuteEventListner();
+});
