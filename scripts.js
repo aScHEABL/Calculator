56564155f99473e46ce40f2b71e007a2154ebaa9
aScHEABL <natothun@gmail.com>
2022-06-28 01:13:25 +0800

    feat: assign function buttons

--- scripts.js
+++ scripts.js
@@ -12,10 +12,16 @@ const numButtons_DOM = numberArray.map((DOM_ID) => document.getElementById(`${DO
 const operatorButtons_DOM = operatorArray.map((DOM_ID) => document.getElementById(`${DOM_ID}`));
 const functionButtons_DOM = functionArray.map((DOM_ID) => document.getElementById(`${DOM_ID}`));
 
+let result = 0;
 screenDisplay_DOM.textContent = ``;
 
 let operatorIsClicked = (index) => {
     console.log(`operator ${operatorArray[index]} is clicked!`);
+
+};
+
+let calculateTheResult = (index) => {
+    console.log(`${functionArray[index]} is clicked!`);
 };
 
 numButtons_DOM.forEach((button, index) => {
@@ -33,6 +39,30 @@ operatorButtons_DOM.forEach((button, index) => {
 
 functionButtons_DOM.forEach((button, index) => {
     button.addEventListener(`click`, () => {
-        console.log(`${functionArray[index]} is clicked!`);
+        switch (index) {
+            case 0: // = is clicked, run the calculation
+                calculateTheResult(index);
+                break;
+            case 1: // AC is clicked, clear both operator and screen display
+                operatorDisplay_DOM.textContent = ``;
+                screenDisplay_DOM.textContent = ``;
+                break;
+            case 2: // backspace is clicked, backspace 1 digit from the screen display
+                screenDisplay_DOM.textContent = screenDisplay_DOM.textContent.slice(0, screenDisplay_DOM.textContent.length - 1);
+                break;
+            case 4: // sign is clicked, check if value is positive, if not turn it into negative, vice versa
+                let stringArray = [];
+                stringArray = screenDisplay_DOM.textContent.split(``);
+                const seeIfNegative = (stringArray[0] === "-") ? true : false;
+                if (seeIfNegative === true) {
+                    stringArray.shift();
+                    screenDisplay_DOM.textContent = stringArray.join("");
+                } else {
+                    stringArray.unshift("-");
+                    screenDisplay_DOM.textContent = stringArray.join("");
+                };
+                console.log(`sign is clicked! new value is: ${stringArray}`);
+                break;
+        };
     });
 });
